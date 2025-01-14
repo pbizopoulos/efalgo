65d018d80d7efd4e8ba44fc3682a8e8b78c2cffd
Paschalis Bizopoulos <pbizop@gmail.com>
2025-01-14 19:31:51 +0200

    .

--- packages/docs/run.sh
+++ packages/docs/run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-cd "$(dirname "$(realpath "$0")")"
-nix run nixpkgs#http-server
-cd -
+if [ -z "${DEBUG}" ]; then
+  cd "$(dirname "$(realpath "$0")")"
+  nix run nixpkgs#http-server
+  cd -
+fi
