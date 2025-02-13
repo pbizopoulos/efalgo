9e21d5c526485d694d4581aabf0c6daa33b21f10
Paschalis Bizopoulos <pbizop@gmail.com>
2025-02-13 10:55:18 +0200

    .

--- flake.nix
+++ flake.nix
@@ -1,22 +1,20 @@
 {
   inputs = {
     canonicalization = {
-      url = "github:pbizopoulos/canonicalization";
       inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:pbizopoulos/canonicalization";
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     snowfall-lib = {
-      url = "github:snowfallorg/lib";
       inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:snowfallorg/lib";
     };
   };
   outputs =
     inputs:
     inputs.snowfall-lib.mkFlake {
       inherit inputs;
+      outputs-builder = _channels: { formatter = inputs.canonicalization.formatter.x86_64-linux; };
       src = ./.;
-      outputs-builder = _channels: {
-        formatter = inputs.canonicalization.formatter.x86_64-linux;
-      };
     };
 }
